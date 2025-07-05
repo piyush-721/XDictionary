d93a865a75004f2af76ca95627e8b4b1730062e4
Piyush Shinde <workwithpiyush77@gmail.com>
2025-07-06 00:30:11 +0530

    Initial commit of XDictionary

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  let data = 
+[
+
+    { word: "React", meaning: "A JavaScript library for building user interfaces." },
+
+    { word: "Component", meaning: "A reusable building block in React." },
+
+    { word: "State", meaning: "An object that stores data for a component." }
+
+];
+
+const [value, setValue] = useState("");
+const [meaning, setMeaning] = useState("");
+
+  const handleSearch = () => {
+    const searchWord = value;
+    for(let i = 0; i < data.length; i++){
+      if(data[i].word === searchWord){
+        setMeaning(data[i].meaning);
+        break;
+      }
+      else{
+        setMeaning("Word not found in the dictionary.");
+      }
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1>Dictionary App</h1>
+
+      <input type="text" value={value} onChange={(e) => setValue(e.target.value)}></input>
+      <button onClick={handleSearch}>Search</button> <br />
+      <h3>Definition:</h3> <p>{meaning}</p>
+
     </div>
   );
 }
